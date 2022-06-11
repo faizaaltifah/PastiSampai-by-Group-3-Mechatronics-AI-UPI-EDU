4eca8c4a3507e32f1038811cfe2ca57e36819859
faizaaltifah <107241239+faizaaltifah@users.noreply.github.com>
2022-06-11 22:29:50 +0700

    Update SQLfile.sql

--- SQLfile.sql
+++ SQLfile.sql
@@ -1,1 +1,11 @@
-
+CREATE DATABASE pasti_sampai;
+USE pasti_sampai;
+CREATE TABLE data_pengiriman (
+`id_pengiriman` int(10) NOT NULL, 
+`tanggal_pengiriman` DATE NOT NULL, 
+`jenis_pengiriman` VARCHAR(10) NOT NULL, 
+`jenis_barang_dikirim` VARCHAR(50) NOT NULL ,
+`asal_pengiriman` VARCHAR(255) NOT NULL ,
+`tujuan_pengiriman` VARCHAR(255) NOT NULL ,
+`status_pengiriman` VARCHAR(20) NOT NULL);
+select * from data_pengiriman
